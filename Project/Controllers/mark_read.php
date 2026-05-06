f71bdd85a8e5a652846fbf9951b12f81302464a7
MOAZ16627 <moazahmedmr@gmail.com>
2026-05-07 02:31:51 +0300

    Update mark_read.php

--- Project/Controllers/mark_read.php
+++ Project/Controllers/mark_read.php
@@ -3,8 +3,7 @@ session_start();
 require_once "../../Controllers/mark_read.php";
 
 if (isset($_SESSION["userid"])) {
-    $db = new DBcontrollers();
-    $db->openconnection();
+    $db = DBcontrollers::getInstance();
     $user_id = $_SESSION["userid"];
     
     $query = "UPDATE notifications SET is_read = 1 WHERE user_id = '$user_id' AND is_read = 0";
