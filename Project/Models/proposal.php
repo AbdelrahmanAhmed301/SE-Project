29912089e0b86bbf2c8663259369c297ef18562c
MOAZ16627 <moazahmedmr@gmail.com>
2026-05-08 03:39:51 +0300

    Update proposal.php

--- Project/Models/proposal.php
+++ Project/Models/proposal.php
@@ -4,12 +4,13 @@ class proposal {
 
 public $proposal_id;
 public $bid_amount;
-public $Delivery_Time;
+public $Delivery_time;
 
 public $freelancer_id;
 public $cover_letter;
 
 public $project_id;
+public $status;
 }
 
 ?>
