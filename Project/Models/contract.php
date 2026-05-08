ec3b686abfce10e9ad4b8e790bf43808a896a4dc
MOAZ16627 <moazahmedmr@gmail.com>
2026-05-08 18:23:24 +0300

    Create contract.php

---
+++ Project/Models/contract.php
@@ -0,0 +1,18 @@
+<?php
+
+class contract {
+
+public $contract_id;
+public $project_id;
+public $freelancer_id;
+
+public $deadline;
+public $revision_limts;
+
+public $revision_used;
+public $create_at;
+
+public $status;
+}
+
+?>
