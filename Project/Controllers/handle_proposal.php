27ccf759416ccaafe9aaacc5a9750c736014e9c3
MOAZ16627 <moazahmedmr@gmail.com>
2026-05-08 18:23:13 +0300

    Update handle_proposal.php

--- Project/Controllers/handle_proposal.php
+++ Project/Controllers/handle_proposal.php
@@ -11,14 +11,31 @@ if (isset($proposal_id) && isset($status)) {
     $db->insertquery($update_query);
 
 
-    if ($status == 'Accepted') {
-        $get_project = $db->Select_query("SELECT project_id FROM proposal WHERE proposal_id = '$proposal_id'");
-        $project_id = $get_project[0]['project_id'];
-        
-        $update_project = "UPDATE projects SET status = 'In Progress' WHERE id = '$project_id'";
-        $db->insertquery($update_project);
-    }
+    
 
+if ($status == 'Accepted') {
+    
+    $proposal_data = $db->Select_query("SELECT * FROM proposal WHERE proposal_id = '$proposal_id'");
+    $project_id = $proposal_data[0]['project_id'];
+    $freelancer_id = $proposal_data[0]['freelancer_id'];
+
+
+    $db->insertquery("UPDATE projects SET status = 'In Progress' WHERE project_id = '$project_id'");
+
+    require_once "../Models/contract.php";
+    require_once "contractController.php";
+
+    $contract = new contract();
+    $contract->project_id = $project_id;
+    $contract->freelancer_id = $freelancer_id;
+    $contract->deadline = $proposal_data[0]['Delivery_time']; 
+    $contract->revision_limts = 3; 
+    $contract->revision_used = 0;
+    $contract->create_at = date("Y-m-d H:i:s");
+
+    $contract_manager = new createcontract();
+    $contract_manager->create_contract($contract);
+}
     header("Location: ../views/Client/client-dashboard.php?msg=StatusUpdated");
 }
 ?>
