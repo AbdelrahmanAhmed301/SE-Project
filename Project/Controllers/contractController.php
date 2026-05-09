6c28795098e9fb44eea1838803ad51874385022d
MOAZ16627 <moazahmedmr@gmail.com>
2026-05-09 18:27:55 +0300

    Update contractController.php

--- Project/Controllers/contractController.php
+++ Project/Controllers/contractController.php
@@ -14,7 +14,7 @@ class createcontract {
         
         $query = "INSERT INTO contract (project_id, freelancer_id, deadline, revision_limits, revision_used, create_at)
                 VALUES ('$contract->project_id', '$contract->freelancer_id', '$contract->deadline', 
-                        '$contract->revision_limits', '$contract->revision_used', '$contract->create_at')";
+                        '$contract->revision_limits','$contract->revision_used', '$contract->create_at')";
         
         $result = $this->db->insertquery($query);
         
@@ -22,14 +22,17 @@ class createcontract {
     }
 
     public function get_contract_details($contract_id) {
-        $query = "SELECT c.*, p.title as project_name, p.budget,
-                u_f.username as freelancer_name, u_c.username as client_name
-                FROM contract c
-                JOIN projects p ON c.project_id = p.project_id
-                JOIN user u_f ON c.freelancer_id = u_f.user_id
-                JOIN user u_c ON p.client_id = u_c.user_id
-                WHERE c.contract_id = '$contract_id'";
-        
-        return $this->db->Select_query($query);
-    }
+    
+    $query = "SELECT c.*, p.title as project_name, 
+            pr.bid_amount as budget, 
+            u_f.username as freelancer_name, u_c.username as client_name
+            FROM contract c
+            JOIN projects p ON c.project_id = p.project_id
+            JOIN proposal pr ON c.project_id = pr.project_id AND c.freelancer_id = pr.freelancer_id
+            JOIN user u_f ON c.freelancer_id = u_f.user_id
+            JOIN user u_c ON p.client_id = u_c.user_id
+            WHERE c.contract_id = '$contract_id' AND pr.status = 'Accepted' LIMIT 1";
+
+    return $this->db->Select_query($query);
+}
 }
