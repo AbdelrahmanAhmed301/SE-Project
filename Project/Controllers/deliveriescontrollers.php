f6928b4907efee50aa3cfc93ea902db8cc59af3e
MOAZ16627 <moazahmedmr@gmail.com>
2026-05-10 02:40:11 +0300

    Create deliveriescontrollers.php

---
+++ Project/Controllers/deliveriescontrollers.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once "DBcontrollers.php";
+
+
+class deliveriescontrollers {
+
+    private $db;
+
+    public function __construct() {
+        $this->db = DBcontrollers::getInstance();
+    }
+
+    public function submit_work(deliveries $deliveries) {
+
+        $query = "
+        INSERT INTO deliveries
+        (project_id, freelancer_id, message, file_path)
+
+        VALUES
+        (
+            '$deliveries->project_id',
+            '$deliveries->freelancer_id',
+            '$deliveries->message',
+            '$deliveries->file_path'
+        )
+        ";
+
+        $result = $this->db->insertquery($query);
+
+        if ($result) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
+?>
