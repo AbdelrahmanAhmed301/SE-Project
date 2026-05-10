e3743fce23605935e4337f54a6335f56ec09a80c
MOAZ16627 <moazahmedmr@gmail.com>
2026-05-11 02:54:20 +0300

    Update notifycontrollers.php

--- Project/Controllers/notifycontrollers.php
+++ Project/Controllers/notifycontrollers.php
@@ -1,6 +1,6 @@
 <?php
-require_once '../../Models/notification.php';
-require_once "../../Controllers/DBcontrollers.php";
+
+require_once "DBcontrollers.php";
 
 class notifycontrollers {
     private $db;
@@ -10,39 +10,42 @@ class notifycontrollers {
     }
 
     public function notify_all_freelancers(notification $notifiy) {
-        if (!$this->db = DBcontrollers::getInstance()) {
-            return false;
-        }
-
-        // 1. جلب كل الـ user_id للمستقلين
         $query = "SELECT user_id FROM user WHERE role_id = 3";
         $freelancers = $this->db->Select_query($query);
 
         if ($freelancers) {
             foreach ($freelancers as $freelancer) {
                 $user_id = $freelancer['user_id'];
-                $created_at = date('Y-m-d H:i:s');
 
                 $checkQuery = "SELECT id FROM notification 
                             WHERE user_id = '$user_id' 
-                            AND title = '$notifiy->title' 
-                            AND msg = '$notifiy->msg' 
+                            AND title = '" . addslashes($notifiy->title) . "' 
+                            AND msg = '" . addslashes($notifiy->msg) . "' 
                             AND create_at > DATE_SUB(NOW(), INTERVAL 1 MINUTE)";
                 
                 $exists = $this->db->Select_query($checkQuery);
-
                 if (!$exists) {
-
                     $insertQuery = "INSERT INTO notification (user_id, title, msg, create_at) 
-                                    VALUES ('$user_id', '$notifiy->title', '$notifiy->msg', '$created_at')";
-                    
+                    VALUES ('$user_id', '" . addslashes($notifiy->title) . "', '" . addslashes($notifiy->msg) . "', NOW())";
                     $this->db->insertquery($insertQuery);
                 }
             }
             return true;
         }
-        
         return false;
     }
+
+    public function get_user_notifications($user_id) {
+        $query = "SELECT * FROM notification WHERE user_id = '$user_id'  create_at ";
+        return $this->db->Select_query($query);
+    }
+    public function add_notification($user_id, $msg) {
+        
+        $query = "INSERT INTO notification (user_id, msg) 
+                VALUES ('$user_id', '$msg')";
+        
+        return $this->db->insertquery($query);
+    }
+
 }
 ?>
