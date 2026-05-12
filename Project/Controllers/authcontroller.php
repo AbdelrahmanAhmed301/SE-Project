30dfb85c44568a0db3770af5ad7e76d2de0cdece
MOAZ16627 <moazahmedmr@gmail.com>
2026-05-12 12:43:37 +0300

    Update authcontroller.php

--- Project/Controllers/authcontroller.php
+++ Project/Controllers/authcontroller.php
@@ -11,25 +11,25 @@ class authcontrollers {
     }
 
     public function login(user $user) {
-        if($this->db = DBcontrollers::getInstance()){
-            
-            $query = "SELECT * FROM user WHERE email='$user->email' AND password_hash='$user->password_hash'";
-            $result = $this->db->Select_query($query);
+    if($this->db = DBcontrollers::getInstance()){
+        $query = "SELECT * FROM user WHERE email='$user->email' LIMIT 1";
+        $result = $this->db->Select_query($query);
 
-            if($result===false || count($result) == 0){
-                // session_start();
-                $_SESSION["errmsg"]="Invalid email or password";
-                return false;
-            } else {
-                $_SESSION["userid"]=$result[0]["user_id"];
-                $_SESSION["username"]=$result[0]["username"];
-                $_SESSION["email"]=$result[0]["email"];
-                $_SESSION["user_roleid"]=$result[0]["role_id"];
+        if($result && count($result) > 0){
+            $user_data = $result[0];
+            if(password_verify($user->password_hash, $user_data['password_hash'])) {
+                $_SESSION["userid"]=$user_data["user_id"];
+                $_SESSION["username"]=$user_data["username"];
+                $_SESSION["email"]=$user_data["email"];
+                $_SESSION["user_roleid"]=$user_data["role_id"];
                 return true;
             }
         }
+        
+        $_SESSION["errmsg"]="Invalid email or password";
+        return false;
     }
-
+}
 public function register(user $user){
     if($this->db = DBcontrollers::getInstance()){
         $query = "INSERT INTO user (username, email, password_hash, role_id)
