eb17cb9b129617959d8a8a7796821e8d1a25bf02
MOAZ16627 <moazahmedmr@gmail.com>
2026-05-12 13:05:07 +0300

    Update fix_admin.php

--- Project/fix_admin.php
+++ Project/fix_admin.php
@@ -1,18 +1,15 @@
 <?php
-// تأكد من صحة المسار لملف الـ DB
+
 require_once "Controllers/DBcontrollers.php"; 
 
 $db = DBcontrollers::getInstance();
 
-// بيانات الأدمن الجديد
 $username = "moaz_admin";
 $email    = "admin@worknest.com";
-$password = "123456"; // ده الباسورد اللي هتدخل بيه
+$password = "123456"; 
 
-// أهم خطوة: تشفير الباسورد عشان password_verify تفهمه
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-// مسح أي أدمن قديم بنفس الإيميل عشان م يحصلش تضارب
 $db->insertquery("DELETE FROM user WHERE email = '$email'");
 
 $sql = "INSERT INTO user (username, email, password_hash, role_id, account_status) 
