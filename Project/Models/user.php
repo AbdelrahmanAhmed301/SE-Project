3c6202b1ebf19d4c59eab50bd76682f5f130e853
MOAZ16627 <moazahmedmr@gmail.com>
2026-05-12 12:43:55 +0300

    Update user.php

--- Project/Models/user.php
+++ Project/Models/user.php
@@ -20,7 +20,14 @@ public $role_id;
 
 
 
-
+public static function isBanned($userId, $db) {
+    $sql = "SELECT account_status FROM user WHERE user_id = '$userId'";
+    $result = $db->Select_query($sql);
+    if ($result && isset($result[0])) {
+        return ($result[0]['account_status'] === 'Banned');
+    }
+    return false;
+}
 
 
 
